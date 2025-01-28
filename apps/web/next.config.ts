706e3da5020670a88e95e030d94e407f4a272634
Oguzhan Kandakoglu <oguzhankandakoglu@gmail.com>
2025-01-28 23:16:36 +0300

    :sparkles: (web) edge setup

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from 'next';
 
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars, node/no-process-env
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
