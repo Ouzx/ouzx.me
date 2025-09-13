44734cd140e4a9fbfc6678387858ce8d7beaa2e3
Oguzhan Kandakoglu <oguzhankandakoglu@gmail.com>
2025-09-13 18:04:33 +0300

    :heavy_minus_sign: (eslint) unused tailwind config removed

--- packages/eslint-config/next.mjs
+++ packages/eslint-config/next.mjs
@@ -1,5 +1,3 @@
-// import tailwindcss from '@yorganci/eslint-plugin-tailwindcss';
-
 import createConfig from './create-config.mjs';
 
 export default createConfig(
@@ -7,12 +5,4 @@ export default createConfig(
     react: true,
     nextjs: true,
   },
-  // tailwindcss.configs.recommended,
-  // {
-  //   settings: {
-  //     tailwindcss: {
-  //       stylesheet: '../../packages/ui/src/styles/globals.css',
-  //     },
-  //   },
-  // },
 );
